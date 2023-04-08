115e6f789b2fb4e8250f38ec86559e8ce4896a82
Exserlot <exserlot01@hotmail.com>
2023-04-08 20:40:32 +0700

    first commit

---
+++ pages/api/stripe..js
@@ -0,0 +1,54 @@
+import Stripe from "stripe";
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const params = {
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        shipping_options: [
+          { shipping_rate: "shr_1MolVTAOg6P7Bu4ircwveXeg" },
+          { shipping_rate: "shr_1MolWsAOg6P7Bu4i538S5RuN" },
+        ],
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img
+            .replace(
+              "image-",
+              "https://cdn.sanity.io/images/ist1b074/production/"
+            )
+            .replace("-webp", ".webp");
+          return {
+            price_data: {
+              currency: "thb",
+              product_data: {
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity,
+          };
+        }),
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
+      };
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+      res.status(200).json(session);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
---
+++ sanity_ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ist1b074',
+    dataset: 'production'
+  }
+})
